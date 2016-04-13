46a141a72028c1214141eb1fc0e840d91552691d
Emeline Tavan <emeline.tavan.pvt@gmail.com>
2016-04-13 09:55:53 +0200

    jquery touchstart click

--- js/javascript.js
+++ js/javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $("nav").hide(700);
         $("#menu").fadeIn(500);
     });
-    $(".fa-times").on('touchstart', function () {
+    $(".fa-times").on('touchstart click', function () {
         $("nav").hide(700);
         $("menu").fadeIn(500);
     });
